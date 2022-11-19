4c28b966f1ad44c57ab95b65934ce8cdd9e80004
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:01:34 +0800

    Update and rename shrtcode.py to shrt_code.py

--- src/shrt_code.py
+++ src/shrt_code.py
@@ -1,33 +1,36 @@
 from requests import get
 
 class ShrtCode:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://api.shrtco.de/v2"
 		self.headers = {
 			"user-agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/103.0.5060.129 Mobile Safari/537.36"
 		}
 	
-	def shorten_url(self, url: str):
+	def shorten_url(self, url: str) -> dict:
 		return get(
 			f"{self.api}/shorten?url={url}",
 			headers=self.headers).json()
 	
-	def protect_url(self, url: str, password: str):
+	def protect_url(
+			self,
+			url: str,
+			password: str) -> dict:
 		return get(
 			f"{self.api}/shorten?url={url}&password={password}",
 			headers=self.headers).json()
 	
-	def generate_emoji_url(self, url: str):
+	def generate_emoji_url(self, url: str) -> dict:
 		return get(
 			f"{self.api}/shorten?emoji&url={url}",
 			headers=self.headers).json()
 			
-	def get_url_information(self, code: str):
+	def get_url_information(self, code: str) -> dict:
 		return get(
 			f"{self.api}/info?code={code}",
 			headers=self.headers).json()
 	
-	def get_status(self):
+	def get_status(self) -> dict:
 		return get(
 			f"{self.api}/status",
 			headers=self.headers).json()
@@ -35,7 +38,7 @@ class ShrtCode:
 	def generate_custom_url(
 			self,
 			url: str,
-			custom_code: str):
+			custom_code: str) -> dict:
 		return get(
 			f"{self.api}/shorten?url={url}&custom_code={custom_code}",
 			headers=self.headers).json()
